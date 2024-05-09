e0cd371ef2d1d10822c57fa0bc11a2794fdec75a
ElectroFoxx <electrofoxxgaming@gmail.com>
2024-05-09 14:25:40 +0200

    Update create_data.cpp

--- create_data.cpp
+++ create_data.cpp
@@ -4,21 +4,39 @@
 #include <ctime>
 #include <iostream>
 #include <fstream>
+#include <cstring>
 
 int main(int argc, char* argv[])
 {
-    std::cout << sizeof(uint8_t) << "\n";
     int N = std::stoi(argv[1]);
+    float chance = std::atof(argv[2]);
     
-    char file_type = argv[2][0];
-    std::ofstream file;
-    if (file_type == 'b')
+    bool text_output = false, binary_output = false;
+    if (strlen(argv[3]) > 1)
     {
-        file = std::ofstream(argv[3], std::ios::binary);
+        if (argv[3][0] == 't' || argv[3][1] == 't')
+            text_output = true;
+        if (argv[3][0] == 'b' || argv[3][1] == 'b')
+            binary_output = true;
     }
     else
+    { 
+        if (argv[3][0] == 'b')
+            binary_output = true;
+        else
+            text_output = true;
+    }
+
+    std::ofstream file_text, file_binary;
+    
+    if (binary_output)
     {
-        file = std::ofstream(argv[3]);
+        file_binary = std::ofstream(std::string(argv[4]) + ".bin", std::ios::binary);
+    }
+
+    if (text_output)
+    {
+        file_text = std::ofstream(std::string(argv[4]) + ".txt");
     }
 
     uint8_t** matrix = new uint8_t*[N];
@@ -37,46 +55,52 @@ int main(int argc, char* argv[])
                 continue;
             }
             
-            if (std::rand() % 10 < 9)
+            if (static_cast<float>(std::rand()) / RAND_MAX >= chance)
             {
                 matrix[i][j] = 0;
                 matrix[j][i] = 0;
             }
             else
             {
-                random_value = std::rand() % 255 + 1;
+                random_value = std::rand() % 100 + 1;
                 matrix[i][j] = random_value;
                 matrix[j][i] = random_value;
             }
         }
     }
 
-    if (file_type == 'b')
+    if (binary_output)
     {
-        file.write(reinterpret_cast<const char*>(&N), sizeof(int));
+        file_binary.write(reinterpret_cast<const char*>(&N), sizeof(int));
         
         for (int i = 0; i < N; i++)
         {
             for (int j = 0; j < N; j++)
             {
-                file.write(reinterpret_cast<const char*>(&matrix[i][j]), sizeof(uint8_t));
+                file_binary.write(reinterpret_cast<const char*>(&matrix[i][j]), sizeof(uint8_t));
             }
         }
     }
-    else
+
+    if (text_output)
     {
         for (int i = 0; i < N; i++)
         {
             for (int j = 0; j < N; j++)
             {
-                file << matrix[i][j] << " ";
+                file_text << static_cast<int>(matrix[i][j]) << " ";
             }
-            file << "\n";
+            file_text << "\n";
         }
     }
 
-    file.close();
+    if (binary_output)
+        file_binary.close();
+
+    if (text_output)
+        file_text.close();
 
     for(int i = 0; i < N; ++i)
         delete[] matrix[i];
+    delete[] matrix;
 }
